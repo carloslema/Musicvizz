d0c2d8094cdd5fb32db694dc2e4f0f80dca9ca43
Suraya Shivji <surayashivji@gmail.com>
2017-01-10 11:29:57 -0800

    week 2

--- sketches/week_02/sketch.js
+++ sketches/week_02/sketch.js
@@ -1,15 +1,74 @@
-const polygon = new mojs.Shape({
-  shape:        'polygon',
-  points:       5,
-  stroke:       '#A8CABA',
-  scale:        { 1 : 1.5 },
-  angle:        { 0 : 360 },
-  fill:         { '#721e5f' : '#a5efce' },
-  radius:       25,
+const burst1 = new mojs.Burst({
+  radius: { 0 : 360 },
+  count: 20,
+  children: {
+    shape: 'cross',
+    stroke: 'teal',
+    strokeWidth: {6 : 0},
+    angle: {360 : 0},
+    radius: {30 : 5},
+    duration: 3000
+  }
+});
 
-  duration:     1500,
-  delay:        100,
-  speed: 1.5,
-  isYoyo:       true, // whether or not tweens back and forth
-  repeat:       999
-}).play();
+const burst2 = new mojs.Burst({
+  radius: { 0 : 150 },
+  count: 10,
+  children: {
+    shape: 'zigzag',
+    stroke: {'magenta' : 'blue'},
+    fill: 'none',
+    strokeWidth: {6 : 0},
+    angle: {'-360' : 0},
+    radius: {30 : 5},
+    opacity: {0.85 : 0},
+    duration: 3000
+  }
+});
+
+const burst3 = new mojs.Burst({
+  radius: { 0 : 300 },
+  count: 14,
+  children: {
+    shape: 'circle',
+    stroke: {'#a9a9a9' : 'black'},
+    strokeWidth: {2 : 0},
+    angle: {'360' : 0},
+    radius: {5 : 0},
+    fill: 'none',
+    duration: 3000
+  }
+});
+
+const burst4 = new mojs.Burst({
+  radius: { 0 : 360 },
+  count: 5,
+  children: {
+    color: 'purple',
+    angle: {'-360' : 0},
+    radius: {10 : 5},
+    opacity: {0.85 : 0},
+    duration: 3000
+  }
+});
+
+const circ = new mojs.Shape({
+  radius: {0 : 200},
+  fill: 'none',
+  stroke: 'yellow',
+  opacity: {1: 0},
+  duration: 2000
+});
+
+const circ2 = new mojs.Shape({
+  radius: {0 : 150},
+  fill: 'none',
+  stroke: 'yellow',
+  opacity: {1: 0},
+  duration: 2000,
+  delay: 100
+});
+
+const timeline = new mojs.Timeline({
+  repeat: 999
+}).add(burst1).add(burst2).add(burst3).add(burst4).add(circ).add(circ2).play();
