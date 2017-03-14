3f1ab720715068a7f8f511a60d53b7040f0202c4
Suraya Shivji <surayashivji@gmail.com>
2017-03-14 11:26:18 -0700

    week 5 done

--- sketches/week_05/sketch.js
+++ sketches/week_05/sketch.js
@@ -1,4 +1,26 @@
-var Cross = function(x, y) {
-  this.x = x;
-  this.y = y;
+tool.minDistance = 40;
+tool.maxDistance = 100;
+tool.distance
+var drawPath;
+
+tool.onMouseDown = function(event) {
+	drawPath = new Path();
+	drawPath.fillColor = new Color({ hue: Math.random() * 360, saturation: 1, brightness: 1 });
+	drawPath.add(event.point);
+}
+
+tool.onMouseDrag = function(event) {
+	var step = event.delta;
+	step.angle += 100;
+	var top = event.middlePoint + step;
+	var bottom = event.middlePoint - step;
+	drawPath.add(top);
+	drawPath.insert(0, bottom);
+	drawPath.smooth();
+}
+
+tool.onMouseUp = function(event) {
+	drawPath.add(event.point);
+	drawPath.closed = true;
+	drawPath.smooth();
 }
