0802593df207e2633b29e9fa02441bd7f82ded9d
Suraya Shivji <surayashivji@gmail.com>
2018-12-01 22:23:03 -0800

    redesign

--- js/title.js
+++
@@ -1,1 +0,0 @@
-(function($){var s,spanizeLetters={settings:{letters:$('.js-spanize'),},init:function(){s=this.settings;this.bindEvents()},bindEvents:function(){s.letters.html(function(i,el){var spanizer=$.trim(el).split("");return'<span>'+spanizer.join('</span><span>')+'</span>'})},};spanizeLetters.init()})(jQuery)
