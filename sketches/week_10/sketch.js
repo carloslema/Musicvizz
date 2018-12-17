6bdd0d00ee8b273b2fa55b07d6dc3fe65d45d048
Suraya Shivji <surayashivji@gmail.com>
2018-12-17 13:02:30 -0800

    added yebba

--- sketches/week_10/sketch.js
+++ sketches/week_10/sketch.js
@@ -19,10 +19,6 @@ function startClicked() {
 	beginAudioProcessing();
 }
 
-function animateStart() {
-	// document.getElementById("start-animate").classList.add('button-animated');
-}
-
 function beginAudioProcessing() {
 	audioClient = new AudioHelper();
 	audioClient.setupAudioProcessing();
--- sketches/week_19/sketch.js
+++ sketches/week_19/sketch.js
@@ -1,121 +1,91 @@
 var audioClient;
-var scene, camera, renderer, clock, controls;
-var controls;
-var proton, emitter;
-var camera, scene, renderer, stats, clock, controls;
-var rate = 0;
-var point = new THREE.Vector2(0.8, 0.5);
+var controls, world, emitter, texture;
+var frequencyData = [0];
+var freqAvg = 0;
+var delta = 0;
+var min = 100;
+var max = -100;
+
 function startClicked() {
   document.getElementsByClassName("button-container")[0].style.visibility = "hidden";
+  document.getElementsByClassName("button-container")[0].style.display = "none";
   document.getElementsByClassName("loader-container")[0].style.visibility = "visible";
   beginAudioProcessing();
 }
 
+world = tQuery.createWorld().boilerplate().start();
+world.tRenderer().setClearColorHex(0x193631, world.tRenderer().getClearAlpha());
+
+texture = THREE.ImageUtils.loadTexture("green.jpg");
+emitter = Fireworks.createEmitter({ nParticles: 5 })
+  .effectsStackBuilder()
+  .spawnerSteadyRate(10)
+  .position(Fireworks.createShapeSphere(0, 0, 0, 2))
+  .velocity(Fireworks.createShapePoint(0, 0, 0))
+  .createEffect('scale', {
+    birthScale: 0.1,
+    mulScale: 0.87
+  }).onBirth(function (particle, deltaTime) {
+    var object3d = particle.get('threejsObject3D').object3d;
+    object3d.scale.set(this.opts.birthScale, this.opts.birthScale, this.opts.birthScale);
+  }).onUpdate(function (particle, deltaTime) {
+    var object3d = particle.get('threejsObject3D').object3d;
+    var newScale;
+    if(freqAvg < 20 || freqAvg > 80) {
+      newScale = 0.8;
+    } else {
+      newScale = freqAvg.map(20, 80, 0.7, 1.5);
+    }
+    newScale = Math.round(newScale * 10) / 10
+    object3d.scale.multiplyScalar(newScale);
+    console.log("scale" + newScale);
+    console.log("avg" + freqAvg);
+    delta = deltaTime;
+  }).back()
+  .friction(0.85)
+  .lifeTime(2, 4)
+  .randomVelocityDrift(Fireworks.createVector(0.2, 0.2, 0.2))
+  .renderToThreejsObject3D({
+    container: world,
+    create: function () {
+      return new THREE.Sprite({
+        map: texture,
+        useScreenCoordinates: false,
+        color: 0xFFFFFF,
+        blending: THREE.AdditiveBlending,
+      });
+    }
+  })
+  .back()
+  .start();
+
+world.loop().hook(function (delta, now) {
+  emitter.update(delta).render();
+});
+
 function beginAudioProcessing() {
   audioClient = new AudioHelper();
   audioClient.setupAudioProcessing();
-  audioClient.loadFile("../../audio/1950.mp3")
+  audioClient.loadFile("../../audio/yeb.mp3")
     .then(init)
     .then(() => {
       audioClient.onAudioProcess(function () {
-        var frequencyData = audioClient.getFrequencyData();
-        var freqAvg = audioClient.getAverage(frequencyData);
-
-        emitter.addBehaviour(new Proton.Rotate(freqAvg, freqAvg));
-        emitter.addBehaviour(new Proton.Scale(1, freqAvg*0.1));
-        emitter.addBehaviour(new Proton.Alpha(1, 0, Infinity, Proton.easeInQuart));
-
-        emitter.addInitialize(new Proton.Life(2, 4));
-        emitter.addInitialize(new Proton.Radius(100));
-
-        // emitter.addInitialize(new Proton.Velocity(freqAvg*2, new Proton.Vector3D(0, 1, 1), 180));
-
-        // emitter.p.x += freqAvg*0.1;
-        // emitter.p.y += freqAvg*0.01;
-
-        // console.log("x: " + emitter.p.x);
-        // console.log("y: " + emitter.p.y);
-      
-        proton.update();
-        renderer.render(scene, camera);
-        camera.lookAt(scene.position);
-
-        rate += 0.02;
-        // console.log("freq" + freqAvg);
-        camera.position.x = Math.sin(rate) + (freqAvg*10);
-        camera.position.z = Math.cos(rate) + (freqAvg*10);
+        frequencyData = audioClient.getFrequencyData();
+        freqAvg = audioClient.getAverage(frequencyData);
       });
     });
 }
 
+// https://gist.github.com/xposedbones/75ebaef3c10060a3ee3b246166caab56
+Number.prototype.map = function (in_min, in_max, out_min, out_max) {
+  return (this - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
 function init() {
-  // SCENE
-  camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
-  camera.position.z = 500;
-
-  scene = new THREE.Scene();
-
-  clock = new THREE.Clock();
-
-  renderer = new THREE.WebGLRenderer();
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.setClearColor("#1B3DAE");
-
-  // LIGHTS
-  var ambientLight = new THREE.AmbientLight(0x101010);
-  scene.add(ambientLight);
-  var pointLight = new THREE.PointLight(0xffffff, 2, 1000, 1);
-  pointLight.position.set(0, 200, 200);
-  scene.add(pointLight);
-
-  document.getElementById("webgl").appendChild(renderer.domElement);
-  document.getElementsByClassName("loader-container")[0].style.visibility = "hidden";
   showControls();
-
-  // PROTON
-  initProton();
+  document.getElementsByClassName("loader-container")[0].style.visibility = "hidden";
+  document.getElementsByClassName("loader-container")[0].style.display = "none";
 };
-
-function initProton() {
-  proton = new Proton();
-  proton.addEmitter(createEmitter());
-  proton.addRender(new Proton.SpriteRender(scene));
-}
-
-function createSprite() {
-  var map = new THREE.TextureLoader().load("../week_07/particle.jpg");
-  var material = new THREE.SpriteMaterial({
-    map: map,
-    color: 0xFD0B0B,
-    blending: THREE.AdditiveBlending,
-    fog: true
-  });
-  return new THREE.Sprite(material);
-}
-
-function createEmitter() {
-  emitter = new Proton.Emitter();
-  emitter.rate = new Proton.Rate(new Proton.Span(5, 10), new Proton.Span(.1, .25));
-  emitter.addInitialize(new Proton.Mass(1));
-  // emitter.addInitialize(new Proton.Radius(100));
-  // emitter.addInitialize(new Proton.Life(2, 4));
-  emitter.addInitialize(new Proton.Body(createSprite()));
-  emitter.addInitialize(new Proton.Position(new Proton.BoxZone(100)));
-  emitter.addInitialize(new Proton.Velocity(250, new Proton.Vector3D(0, 1, 1), 180));
-
-  // //emitter.addBehaviour(new Proton.RandomDrift(30, 30, 30, .05));
-  // emitter.addBehaviour(new Proton.Rotate("random", "random"));
-  // emitter.addBehaviour(new Proton.Scale(1, 0.5));
-  // emitter.addBehaviour(new Proton.Alpha(1, 0, Infinity, Proton.easeInQuart));
-
-  emitter.addBehaviour(new Proton.Color(0xFD0B0B, 0xFFE347, Infinity, Proton.easeOutQuart));
-  emitter.p.x = 0;
-  emitter.p.y = 0;
-  emitter.emit();
-  return emitter;
-}
-
 
 document.getElementById("mute").onclick = function () {
   toggleMuteControl();
